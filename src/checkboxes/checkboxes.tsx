232f8a4b5d0da35c587af33b690fb6f8ac9baeaf
kaidornel <kai_dornel@hotmail.com>
2022-03-09 20:36:42 -0500

    added checkbox

---
+++ src/checkboxes/checkboxes.tsx
@@ -0,0 +1,16 @@
+
+
+const Checkboxes = (props: any) => (
+    <div className="filter-checkboxes">
+        <nav id="filter-group" className="filter-group">
+        <input onChange={props.toggleVisibility} name="community" type="checkbox" id="poi-theatre" defaultChecked></input>
+        <label htmlFor='poi-theatre'>Community</label>
+        <input onChange={props.toggleVisibility} name="research" type="checkbox" id="poi-rocket" defaultChecked></input>
+        <label htmlFor='poi-rocket'>Research</label>
+        <input onChange={props.toggleVisibility} name="government" type="checkbox" id="poi-museum" defaultChecked></input>
+        <label htmlFor='poi-museum'>Government</label>
+        </nav>
+    </div>
+);
+
+export default Checkboxes;
