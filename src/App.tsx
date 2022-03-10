bed25f6c13a4b1a3fab6cd10b2630919a0c82754
kaidornel <kai_dornel@hotmail.com>
2022-03-09 20:36:23 -0500

    building routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css';
-// import * as mapboxgl from '!mapbox-gl';
+// import { Route, Switch, withRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import MainWeatherDisplayContainer from './main-weather-display/main-weather-display-container';
+import LocationMeasurements from './main-weather-display/location-measurements/location-measurements';
 
 function App() {
   return (
     <div className="App">
       <h1>App loads!</h1>
       <MainWeatherDisplayContainer/>
+      <Link to="/measurements/:locationId">Measurement Data</Link> |{" "}
+      <LocationMeasurements />  
+      {/* <Switch>
+        <Route path="/measurements/:locationId" />
+      </Switch> */}
     </div>
   );
 }
 
 export default App;
+// export default withRouter(App);
