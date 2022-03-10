dc33c311385d897cd42c0a820f9bf2e1aa293191
kaidornel <kai_dornel@hotmail.com>
2022-03-09 20:38:18 -0500

    did it with react

--- src/main-weather-display/world-map/map-of-usa.tsx
+++ src/main-weather-display/world-map/map-of-usa.tsx
@@ -1,21 +1,23 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, ChangeEventHandler } from 'react';
 import './map-of-usa.css';
 import mapboxgl, { MapboxGeoJSONFeature } from 'mapbox-gl';
 import { FeatureCollection, GeoJsonProperties, Geometry } from 'geojson';
 import { Feature } from 'geojson';
-import Popup from '../popup/popup';
+import Popup from '../location-measurements/location-measurements';
 import ReactDOM from 'react-dom';
+import { ChangeEvent } from 'react';
+import Checkboxes from '../../checkboxes/checkboxes';
+import { CheckboxButton } from '../../enum/enums';
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2Rvcm5lbCIsImEiOiJjbDBidTE5Z3YxMHE0M2NtbjN5ZzJkMDk1In0.kRcnZyDcmcDMs7DvPmXvGg';
 
 const MapOfUsa = (props: any) => {
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
     const popupRef = useRef(new mapboxgl.Popup({ offset: 15 }));
-    // const popup = new mapboxgl.Popup({ offset: 25 }).setDOMContent(popupRef.current); 
 
     const [lng, setLng] = useState(5);
     const [lat, setLat] = useState(34);
     const [zoom, setZoom] = useState(1.5);
-  
+    const [map, setMap] = useState<mapboxgl.Map>({} as any);
     // Initialize map when component mounts
     useEffect(() => {
       const map: mapboxgl.Map = new mapboxgl.Map({
@@ -60,8 +62,9 @@ const MapOfUsa = (props: any) => {
                             <div>
                                 <strong>${props.airQualityData.current[i].entity}</strong>
                                 <div>
-                                    <button class="see-more-button" id="see-more-button">See more</button>
+                                    <button class="more-info-button" id="more-info-button">More info</button>
                                     <p>id: ${props.airQualityData.current[i].id}</p>
+                                    <p>Country: ${props.airQualityData.current[i].country || 'N/A'}<p/>
                                     <p>City: ${props.airQualityData.current[i].city || 'N/A'}<p/>
                                     <p>Location: ${props.airQualityData.current[i].name || 'N/A'}<p/>
                                     <p>Sensor Type: ${props.airQualityData.current[i].sensorType}<p/>
@@ -90,7 +93,6 @@ const MapOfUsa = (props: any) => {
         for (const feature of places.features) {
           const symbol = feature.properties?.icon;
           const LayerID = `poi-${symbol}`;
-
           // Add a layer for this symbol type if it hasn't been added already.
           if (!map.getLayer(LayerID)) {
               map.addLayer({
@@ -121,14 +123,14 @@ const MapOfUsa = (props: any) => {
                       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                     }
 
-                    // create popup node
-                    const popupNode = document.createElement("div")
-                    ReactDOM.render(
-                      <Popup
-                        feature={feature}
-                      />,
-                      popupNode
-                    )
+                    // // create popup node
+                    // const popupNode = document.createElement("div")
+                    // ReactDOM.render(
+                    //   <Popup
+                    //     feature={feature}
+                    //   />,
+                    //   popupNode
+                    // )
 
                     popupRef.current
                       .setLngLat(coordinates)
@@ -149,16 +151,16 @@ const MapOfUsa = (props: any) => {
                     map.getCanvas().style.cursor = '';
                 });
 
-                map.on('mousemove', (e: any) => {
-                  const infoElement: any = document.getElementById('info')
-                  infoElement.innerHTML =
-                        // `e.point` is the x, y coordinates of the `mousemove` event
-                        // relative to the top-left corner of the map.
-                        JSON.stringify(e.point) +
-                        '<br />' +
-                        // `e.lngLat` is the longitude, latitude geographical position of the event.
-                        JSON.stringify(e.lngLat.wrap());
-                });
+                // map.on('mousemove', (e: any) => {
+                //   const infoElement: any = document.getElementById('info')
+                //   infoElement.innerHTML =
+                //         // `e.point` is the x, y coordinates of the `mousemove` event
+                //         // relative to the top-left corner of the map.
+                //         JSON.stringify(e.point) +
+                //         '<br />' +
+                //         // `e.lngLat` is the longitude, latitude geographical position of the event.
+                //         JSON.stringify(e.lngLat.wrap());
+                // });
               }
             }
     });
@@ -168,22 +170,44 @@ const MapOfUsa = (props: any) => {
         setLat(Number(map.getCenter().lat.toFixed(4)));
         setZoom(Number(map.getZoom().toFixed(2)));
       });
-  
+      setMap(map);
       // Clean up on unmount
       return () => map.remove();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
   
-    return (
-      <div>
-        <div className='sidebarStyle'>
-          <div>
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-          </div>
+  const toggleVisibility = (event: ChangeEvent<HTMLInputElement>) => {
+    let layerId: string = '';
+    switch(event.target.name) {
+      case CheckboxButton.Community:
+        layerId = 'poi-theatre'
+        break;
+      case CheckboxButton.Research:
+        layerId = 'poi-rocket'
+        break;
+      case CheckboxButton.Government:
+        layerId = 'poi-museum'
+        break;
+    }
+    map.setLayoutProperty(
+      layerId,
+      'visibility',
+      event.target.checked ? 'visible' : 'none',
+    )
+  }
+
+  return (
+    <div>
+      <div className='sidebarStyle'>
+        <div>
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
         </div>
-        <div className='map-container' ref={mapContainerRef} />
-        <pre id="info"></pre>
       </div>
-    );
+      <div className='map-container' ref={mapContainerRef} />
+      <Checkboxes
+        toggleVisibility={toggleVisibility}
+      />
+    </div>
+  );
 };
 
 export default MapOfUsa;
