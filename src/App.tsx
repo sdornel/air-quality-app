aae283b05af878e9079d5fd9630075580cb1beef
kaidornel <kai_dornel@hotmail.com>
2022-03-11 11:15:31 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-// import { Route, Switch, withRouter } from "react-router-dom";
 import { BrowserRouter, Link } from "react-router-dom";
 import MainWeatherDisplayContainer from './main-weather-display/main-weather-display-container';
 import LocationMeasurements from './main-weather-display/location-measurements/location-measurements';
@@ -9,11 +8,6 @@ function App() {
     <div className="App">
       <h1>App loads!</h1>
       <MainWeatherDisplayContainer/>
-      <Link to="/measurements/:locationId">Measurement Data</Link> |{" "}
-      <LocationMeasurements />  
-      {/* <Switch>
-        <Route path="/measurements/:locationId" />
-      </Switch> */}
     </div>
   );
 }
