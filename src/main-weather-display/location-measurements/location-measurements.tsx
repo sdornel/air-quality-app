bb24fa966a2f2f1ac68492cd0653627c5a68ee8e
kaidornel <kai_dornel@hotmail.com>
2022-03-11 11:16:48 -0500

    can pass down state with props

--- src/main-weather-display/location-measurements/location-measurements.tsx
+++ src/main-weather-display/location-measurements/location-measurements.tsx
@@ -1,16 +1,20 @@
-const LocationMeasurements = (props: any) => (
-    <div className="popup">
-      {/* <h3 className="route-name">{routeName}</h3> */}
+import { useLocation } from "react-router-dom";
+
+const LocationMeasurements = (props: any) => {
+  console.log('props', props);
+  const { state } = useLocation();
+  console.log(state);
+  // debugger
+  return (
+    <div className="location-measurements">
       <div className="route-metric-row">
         <h4 className="row-title">Route #</h4>
-        {/* <div className="row-value">{routeNumber}</div> */}
       </div>
       <div className="route-metric-row">
         <h4 className="row-title">Route Type</h4>
-        {/* <div className="row-value">{type}</div> */}
       </div>
-      {/* <p className="route-city">Serves {city}</p> */}
     </div>
-)
+  )
+}
 
-export default LocationMeasurements
+export default LocationMeasurements;
