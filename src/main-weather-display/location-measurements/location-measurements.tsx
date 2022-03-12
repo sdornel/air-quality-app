16f42f9bd3a3b25c81340a3a256fd0789a5b0723
kaidornel <kai_dornel@hotmail.com>
2022-03-12 11:15:27 -0500

    todo list

--- src/main-weather-display/location-measurements/location-measurements.tsx
+++ src/main-weather-display/location-measurements/location-measurements.tsx
@@ -1,10 +1,13 @@
 import { useLocation } from "react-router-dom";
 
 const LocationMeasurements = (props: any) => {
-  console.log('props', props);
   const { state } = useLocation();
   console.log(state);
-  // debugger
+  // TODO: make graphs to show data
+  // make cards with all the data for each individual measurement
+  // make a back button to go back to main page
+  // either at a page level or within one of the visual representations, 
+  // build or incorporate a selection tool to filter and further analyze the data
   return (
     <div className="location-measurements">
       <div className="route-metric-row">
