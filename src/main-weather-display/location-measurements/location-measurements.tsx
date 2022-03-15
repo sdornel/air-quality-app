4f0e993f90784be37923ad942992fd1539b1b428
kaidornel <kai_dornel@hotmail.com>
2022-03-14 21:57:55 -0400

    created "cards"

--- src/main-weather-display/location-measurements/location-measurements.tsx
+++ src/main-weather-display/location-measurements/location-measurements.tsx
@@ -1,21 +1,37 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import './location-measurements.css'
 
 const LocationMeasurements = (props: any) => {
-  const { state } = useLocation();
+  const { state }: any = useLocation();
   console.log(state);
-  // TODO: make graphs to show data
-  // make cards with all the data for each individual measurement
-  // make a back button to go back to main page
+  // TODO: 
+  // make graphs to show data
+  // make cards with data for each individual measurement DONE
+  // make a back button to go back to main page DONE
   // either at a page level or within one of the visual representations, 
   // build or incorporate a selection tool to filter and further analyze the data
+
+  // const measurementData = await props.getMeasurementData(selectedLocationId.current);
+  // props.navigateToMeasurements(`/measurements/${Number(selectedLocationId.current)}`, {
+  //   state: {
+  //     measurementData
+  //   },
+
   return (
-    <div className="location-measurements">
-      <div className="route-metric-row">
-        <h4 className="row-title">Route #</h4>
-      </div>
-      <div className="route-metric-row">
-        <h4 className="row-title">Route Type</h4>
-      </div>
+    <div className="location-measurements-div">
+      <Link to="/"><button>Back</button></Link>
+      <h2 className="location-measurements-title">Measurements for Location</h2>
+      {state.measurementData.results[0].parameters.map((measurement: any, index: any) => {
+        return (
+          <div key={index}>
+            <p>Unit: {measurement.unit}</p>
+            <p>Last Value: {measurement.parameter}</p>
+            <p>Last Value: {measurement.displayName}</p>
+            <p>Last Value: {measurement.lastValue}</p>
+            <span>---------------------------------</span>
+          </div>
+        )
+      })}
     </div>
   )
 }
