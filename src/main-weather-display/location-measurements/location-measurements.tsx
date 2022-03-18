1bea97356dc6aafcd03e6616c31c7049b6b6f652
kaidornel <kai_dornel@hotmail.com>
2022-03-17 21:49:28 -0400

    graph works. finished todo (more or less)

--- src/main-weather-display/location-measurements/location-measurements.tsx
+++ src/main-weather-display/location-measurements/location-measurements.tsx
@@ -1,31 +1,17 @@
 import { useLocation, Link } from "react-router-dom";
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import './location-measurements.css';
 
 const LocationMeasurements = (props: any) => {
   const { state }: any = useLocation();
   console.log(state);
-  // TODO: 
-  // make graphs to show data
-  // make cards with data for each individual measurement DONE
-  // make a back button to go back to main page DONE
-  // either at a page level or within one of the visual representations, 
-  // build or incorporate a selection tool to filter and further analyze the data
 
-  // const measurementData = await props.getMeasurementData(selectedLocationId.current);
-  // props.navigateToMeasurements(`/measurements/${Number(selectedLocationId.current)}`, {
-  //   state: {
-  //     measurementData
-  //   },
-
-  const data = [{name: 'Page A', uv: 0.5, pv: 2400, amt: 2400}, {name: 'Page B', uv: 10, pv: 2400, amt: 2400}, {name: 'Page C', uv: 5, pv: 2400, amt: 2400}];
-
-  // const renderLineChart = (
-    // <LineChart width={400} height={400} data={data}>
-    //   <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    // </LineChart>
-  // );
-
+  const data: any[] = [];
+  state.measurementData.results[0].parameters.forEach((param: any) => {
+    if (param.unit === 'µg/m³') {
+      data.push({ name: param.displayName, uv: param.average, pv: 2400, amt: 2400 })
+    }
+  });
 
   return (
     <div className="location-measurements-div">
@@ -35,15 +21,19 @@ const LocationMeasurements = (props: any) => {
         return (
           <div key={index}>
             <p>Unit: {measurement.unit}</p>
-            <p>Last Value: {measurement.parameter}</p>
-            <p>Last Value: {measurement.displayName}</p>
+            <p>Parameter: {measurement.parameter}</p>
+            <p>Display Name: {measurement.displayName}</p>
             <p>Last Value: {measurement.lastValue}</p>
             <span>---------------------------------</span>
           </div>
         )
       })}
-      <LineChart width={400} height={400} data={data}>
+      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        <Tooltip />
       </LineChart>
     </div>
   )
