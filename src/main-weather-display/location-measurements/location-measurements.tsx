5646f51dafd793b736cc7c01aa1447c6cad4e796
kaidornel <kai_dornel@hotmail.com>
2022-03-19 12:42:10 -0400

    made new graph. centered stuff

--- src/main-weather-display/location-measurements/location-measurements.tsx
+++ src/main-weather-display/location-measurements/location-measurements.tsx
@@ -3,41 +3,59 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import './location-measurements.css';
 
 const LocationMeasurements = (props: any) => {
-  // TODO: put graph in middle. make it big
+  // TODO:
   // have more explanation on page with world map
+  // loading screen for when trying to view map
   // figure out the typing
   // unit tests
   const { state }: any = useLocation();
-
-  const data: any[] = [];
+  console.log('state', state);
+  const data1: any[] = [];
+  const data2: any[] = [];
   state.measurementData.results[0].parameters.forEach((param: any) => {
     if (param.unit === 'µg/m³') {
-      data.push({ name: param.displayName, uv: param.average, pv: 2400, amt: 2400 })
+      data1.push({ name: param.displayName, uv: param.average, pv: 2400, amt: 2400 })
+    }
+    if (param.unit === 'particles/cm³') {
+      data2.push({ name: param.displayName, uv: param.average, pv: 2400, amt: 2400 })
     }
   });
 
   return (
-    <div className="location-measurements-div">
+    <div className="location-measurements-wrapper-div">
       <Link to="/"><button>Back</button></Link>
-      <h2 className="location-measurements-title">Measurements for Location</h2>
-      {state.measurementData.results[0].parameters.map((measurement: any, index: any) => {
-        return (
-          <div key={index}>
-            <p>Unit: {measurement.unit}</p>
-            <p>Parameter: {measurement.parameter}</p>
-            <p>Display Name: {measurement.displayName}</p>
-            <p>Last Value: {measurement.lastValue}</p>
-            <span>---------------------------------</span>
-          </div>
-        )
-      })}
-      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="name" />
-          <YAxis />
-        <Tooltip />
-      </LineChart>
+      <div className="location-measurements-div">
+        <div className="cards-div">
+          <h2 className="location-measurements-title">Measurements for Location</h2>
+          {state.measurementData.results[0].parameters.map((measurement: any, index: any) => {
+            return (
+              <div key={index}>
+                <p>Unit: {measurement.unit}</p>
+                <p>Parameter: {measurement.parameter}</p>
+                <p>Display Name: {measurement.displayName}</p>
+                <p>Last Value: {measurement.lastValue}</p>
+                <span>---------------------------------</span>
+              </div>
+            )
+          })}
+        </div>
+        <div className="graph-div">
+          <LineChart width={900} height={400} data={data1} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+            <Tooltip />
+          </LineChart>
+          <LineChart width={900} height={400} data={data2} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+            <Tooltip />
+          </LineChart>
+        </div>
+      </div>
     </div>
   )
 }
