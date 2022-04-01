00ce1e238e8667eefce381fd55354e1a95551b8f
kaidornel <kai_dornel@hotmail.com>
2022-04-01 19:39:20 -0400

    tests for checkboxes

---
+++ src/checkboxes/checkboxes.spec.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import shallow from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import { render, screen } from '@testing-library/react';
+import Checkboxes from './checkboxes';
+
+describe('Checkboxes', () => {
+  describe('community', () => {
+    it('renders community text and input button', async () => {
+      render(<Checkboxes />);
+      const communityText = screen.queryByText('Community');
+      expect(communityText).toBeVisible();
+      
+      const communityButton = await screen.findByTitle('community-button');
+      expect(communityButton).toBeInTheDocument();
+    });
+  });
+
+  describe('research', () => {
+    it('renders research text and input button', async () => {
+      render(<Checkboxes />);
+      const researchText = screen.queryByText('Research');
+      expect(researchText).toBeVisible();
+      
+      const researchButton = await screen.findByTitle('research-button');
+      expect(researchButton).toBeInTheDocument();
+    });
+  });
+
+  describe('government', () => {
+    it('renders government text and input button', async () => {
+      render(<Checkboxes />);
+      const governmentText = screen.queryByText('Government');
+      expect(governmentText).toBeVisible();
+      
+      const governmentButton = await screen.findByTitle('government-button');
+      expect(governmentButton).toBeInTheDocument();
+    });
+  });
+});
--- src/checkboxes/checkboxes.tsx
+++ src/checkboxes/checkboxes.tsx
@@ -1,13 +1,13 @@
 import './checkboxes.css';
 
 const Checkboxes = (props: any) => (
-    <div className="filter-checkboxes-div">
+    <div className="filter-checkboxes-div" data-testid="filter-checkboxes-div">
         <nav id="filter-group" className="filter-group">
-        <input onChange={props.toggleVisibility} name="community" type="checkbox" id="poi-theatre" defaultChecked></input>
+        <input onChange={props.toggleVisibility} title="community-button" name="community" type="checkbox" id="poi-theatre" defaultChecked></input>
         <label htmlFor='poi-theatre'>Community</label>
-        <input onChange={props.toggleVisibility} name="research" type="checkbox" id="poi-rocket" defaultChecked></input>
+        <input onChange={props.toggleVisibility} title="research-button" name="research" type="checkbox" id="poi-rocket" defaultChecked></input>
         <label htmlFor='poi-rocket'>Research</label>
-        <input onChange={props.toggleVisibility} name="government" type="checkbox" id="poi-museum" defaultChecked></input>
+        <input onChange={props.toggleVisibility} title="government-button" name="government" type="checkbox" id="poi-museum" defaultChecked></input>
         <label htmlFor='poi-museum'>Government</label>
         </nav>
     </div>
