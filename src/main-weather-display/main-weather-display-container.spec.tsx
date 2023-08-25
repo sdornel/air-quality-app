0aa752689b7dcc72048ed69a8e611aa2d3f7585b
kaidornel <kai_dornel@hotmail.com>
2023-08-25 10:18:18 -0400

    commit old stuff

--- src/main-weather-display/main-weather-display-container.spec.tsx
+++ src/main-weather-display/main-weather-display-container.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { shallow, configure } from 'enzyme';
 import MainWeatherDisplayContainer from './main-weather-display-container';
+import onLoad from "./main-weather-display-container";
 import Adapter from 'enzyme-adapter-react-16';
 import { BrowserRouter, useNavigate } from 'react-router-dom';
 import * as React from 'react';
@@ -29,7 +30,34 @@ describe('MainWeatherDisplayContainer', () => {
 
   describe('data loading', () => {
     it('renders loading screen', async () => {
+        shallow(component);
+        // onLoad();
+        // const setLoading = jest.fn();
+        // const useStateSpy = jest.spyOn(React, 'useState');
+        // useStateSpy.mockImplementation((loading) => [loading, setLoading]);
+        // React.useState = jest.fn();
+        // jest
+        //     .spyOn(React, 'useState') 
+        //     .mockImplementationOnce(() => [setLoading, () => null])
+        // // Cache original functionality
+        // const realUseState = React.useState
 
+        // // Stub the initial state
+        // const stubInitialState = false;
+
+        // // Mock useState before rendering your component
+        // jest
+        // .spyOn(React, 'useState')
+        // .mockImplementationOnce(() => realUseState(stubInitialState));       
+            
+        const mockSetState = jest.fn();
+
+        jest.mock('react', () => ({
+            useState: initial => [initial, mockSetState]
+          }));
+
+        const loadingText = screen.queryByText('Loading. Please wait.');
+        expect(loadingText).toBeVisible();
     });
   });
 });
