927166c70ccfe43d231567cb805a009615e3ebfb
kaidornel <kai_dornel@hotmail.com>
2023-08-25 19:29:21 -0400

    create routes file

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const routes = require('./routes/routes.js');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello, Express!');
+// });
+
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
