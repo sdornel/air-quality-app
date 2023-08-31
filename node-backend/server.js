414f335bef136c435b824b7ffb4274f1d059c734
kaidornel <kai_dornel@hotmail.com>
2023-08-31 12:19:41 -0400

    cors

--- node-backend/server.js
+++ node-backend/server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes.js');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
-
+app.use(cors());
 // app.get('/', (req, res) => {
 //     res.send('Hello, Express!');
 // });
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 
-               "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", 
-               "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) => {
+    // res.header("Access-Control-Allow-Origin", 
+    //            "http://localhost:4200");
+    // res.header("Access-Control-Allow-Headers", 
+    //            "Origin, X-Requested-With, Content-Type, Accept");
+    // next();
+// });
 
 // app.use(express.static(path.join(__dirname, './dist/angular-frontend')));
 
